a95da4b85aef6fc7dda27d5cee58abd4268aaa07
robertoc1415 <70409108+robertoc1415@users.noreply.github.com>
2023-03-04 14:25:50 -0500

    maquina con servicio apache 2 instalado

--- vmgcp.tf
+++ vmgcp.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
 
@@ -24,10 +24,25 @@ resource "google_compute_instance" "default" {
       // Aquí se está utilizando una dirección IP dinámica
     }
   }
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install -y apache2"
 
   # Configuración del firewall para permitir el tráfico HTTP y HTTPS
   tags = ["http-server", "https-server"]
 }
+
+# # Recurso de null_resource para aprovisionamiento con Ansible
+# resource "null_resource" "default" {
+#   # Dependencia del recurso de instancia de VM para asegurar que se cree primero
+#   depends_on = [maquina1.default]
+
+#   # Configuración de aprovisionamiento de Ansible
+#   provisioner "local-exec" {
+#     command     = "ansible-playbook -i '${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip},' playbook.yaml"
+#     working_dir = "${path.module}/ansible"
+#   }
+# }
+
+
 
 # Configuración del firewall
 resource "google_compute_firewall" "default" {
--- vmgcp_variables.tfvars
+++ vmgcp_variables.tfvars
@@ -1,6 +1,6 @@
 project_id = "miprimeravez"
 region = "us-central1"
 zone = "us-central1-a"
-myvmname = "maquina1"
+myvmname = "cacao3"
 myvmtype = "e2-micro"
 myfirewallname = "controlador"
